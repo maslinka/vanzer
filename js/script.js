4ddb390b4b73a9c2f4c0dbadab0f5c879303c773
unknown <you@example.com>
2019-07-11 19:52:17 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function($) {
-	! function () {
+
 		var e;
 		window.addEventListener("load", function () {
 			e = new google.maps.Map(document.getElementById("map"), {
@@ -38,131 +38,85 @@ $(document).ready(function($) {
 				icon: "src/img/marker.png"
 			})
 		})
-	}();
-	//# sourceMappingURL=map-dist.js.map
-
-// $('.autoplay-i').slick({
-// 	slidesToShow: 3,
-// 	slidesToScroll: 1,
-// 	autoplay: true,
-// 	autoplaySpeed: 5000,
-// 	dots: true,
-// 	pauseOnHover: true,
-// 	arrows: false,
-// });
-// $('.autoplay-w').slick({
-// 	slidesToShow: 3,
-// 	slidesToScroll: 1,
-// 	autoplay: true,
-// 	autoplaySpeed: 5000,
-// 	dots: true,
-// 	pauseOnHover: true,
-// 	arrows: false,
-// });
-$('.carousel').slick({
-	slidesToShow: 3,
-	slidesToScroll: 1,
-	autoplay: true,
-	autoplaySpeed: 2000,
-	dots: true,
-	pauseOnHover: true,
-	arrows: false,
-	// focusOnSelect: true,
-	// centerMode: true
-	responsive: [
-		{
-		  breakpoint: 1024,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			infinite: true,
-			dots: true
-		  }
-		},
-	
-		{
-		  breakpoint: 900,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-	
-		  }
-		},
-		{
-		  breakpoint: 480,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1,
-		  }
-		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-	  ]
-});
-
-$("#fullpage").fullpage({
-	scrollingSpeed: 700,
-	verticalCentered: false,
-	navigation: true,
-	navigationPosition: 'left',
-	// navigationTooltips: ['Header', 'About', 'Services', 'Portfolio', 'Blog', 'Contact'],
-	sectionsColor: ['#ccc', '#fff', '#ccc', '#ccc', '#ccc'],
-	responsiveWidth:900,
-
-	afterLoad: function(anchorLink, index) {
-		var isAnimatedHeader = $('.header .is-animated');
-
-		if(index == 1){
-			isAnimatedHeader.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1s'}); // nav
-			isAnimatedHeader.eq(1).addClass('animated fadeIn').css({'animation-delay': '1.5s', 'animation-duration': '1.5s'}); // title
-			isAnimatedHeader.eq(2).addClass('animated fadeIn').css({'animation-delay': '1.5s', 'animation-duration': '1.5s'}); // subtitle
-		}
-	},
-
-	onLeave: function(index, nextIndex, direction) {
-
-		var isAnimatedAbout = $('.about .is-animated'),
-		isAnimatedServices = $('.services .is-animated'),
-		isAnimatedPortfolio = $('.portfolio .is-animated'),
-		// isAnimatedBlog = $('.blog .is-animated'),
-		isAnimatedContact = $('.contact .is-animated');
-		// 'use strict';
-
-		if(nextIndex == 2) {
-			isAnimatedAbout.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); //title		
-		}
-
-		if(nextIndex == 3) {
-			isAnimatedServices.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); // title
-
-		}
-
-		if(nextIndex == 4) {
-			isAnimatedPortfolio.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); // title
-			isAnimatedPortfolio.eq(1).addClass('animated fadeIn').css({'animation-delay': '1.5s', 'animation-duration': '1.5s'}); // slider
-		}
-
-		// if(nextIndex == 5) {
-		// 	isAnimatedBlog.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); // title
-		// }
-
-		 if(nextIndex == 5) {
-			isAnimatedContact.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); // title
 		
-		 }
-	}
-});
+		$("#fullpage").fullpage({
+			scrollingSpeed: 700,
+			verticalCentered: false,
+			navigation: true,
+			navigationPosition: 'left',
+			// navigationTooltips: ['Header', 'About', 'Services', 'Portfolio', 'Blog', 'Contact'],
+			sectionsColor: ['#ccc', '#fff', '#ccc', '#ccc', '#ccc'],
+			responsiveWidth:800,
+		
+			afterLoad: function(anchorLink, index) {
+				var isAnimatedHeader = $('.header .is-animated');
+		
+				if(index == 1){
+					isAnimatedHeader.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1s'}); // nav
+					isAnimatedHeader.eq(1).addClass('animated fadeIn').css({'animation-delay': '1.5s', 'animation-duration': '1.5s'}); // title
+					isAnimatedHeader.eq(2).addClass('animated fadeIn').css({'animation-delay': '1.5s', 'animation-duration': '1.5s'}); // subtitle
+				}
 
-
-
-
-// $('#btnDisableFP').on( "click", destroyFP);
-
-// function destroyFP() {
-// $("#fullpage").fullpage.destroy();
-// $('body').css("overflow-x","hidden");
-//  }
+			},
+			afterRender: function(){
+				if ($('.carousel').length) {
+					$('.carousel').slick({
+						slidesToShow: 3,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 2000,
+						dots: true,
+						pauseOnHover: true,
+						arrows: false,
+						// focusOnSelect: true,
+						// centerMode: true
+						responsive: [
+							{
+							  breakpoint: 1024,
+							  settings: {
+								slidesToShow: 1,
+								infinite: true,
+								dots: true
+							  }
+							}
+						  ]
+					});
+				}
+			},
+			onLeave: function(index, nextIndex, direction) {
+		
+				var isAnimatedAbout = $('.about .is-animated'),
+				isAnimatedServices = $('.services .is-animated'),
+				isAnimatedPortfolio = $('.portfolio .is-animated'),
+				// isAnimatedBlog = $('.blog .is-animated'),
+				isAnimatedContact = $('.contact .is-animated');
+				// 'use strict';
+		
+				if(nextIndex == 2) {
+					isAnimatedAbout.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); //title		
+				}
+		
+				if(nextIndex == 3) {
+					isAnimatedServices.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); // title
+		
+				}
+		
+				if(nextIndex == 4) {
+					
+					isAnimatedPortfolio.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); // title
+					isAnimatedPortfolio.eq(1).addClass('animated fadeIn').css({'animation-delay': '1.5s', 'animation-duration': '1.5s'}); // slider
+				}
+		
+				// if(nextIndex == 5) {
+				// 	isAnimatedBlog.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); // title
+				// }
+		
+				 if(nextIndex == 5) {
+					isAnimatedContact.eq(0).addClass('animated fadeInDown').css({'animation-delay': '.5s', 'animation-duration': '1.5s'}); // title
+				
+				 }
+			}
+		});
 
  });
 
